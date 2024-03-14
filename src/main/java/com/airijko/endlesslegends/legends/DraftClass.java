a34a7c6d6029284ed4b828a145ea244e51a17343
Airijko <airijkoblade@gmail.com>
2024-03-14 18:28:08 -0500

    test class

--- src/main/java/com/airijko/endlesslegends/legends/DraftClass.java
+++ src/main/java/com/airijko/endlesslegends/legends/DraftClass.java
@@ -2,6 +2,6 @@ package com.airijko.endlesslegends.legends;
 
 public class DraftClass extends Legend {
     public DraftClass(Rank rank) {
-        super(50, 50, 10, 0.5, 10, 10, 50 , 100, rank);
+        super(50, 50, 10, 0.5, 0.1, 10, 50 , 100, rank);
     }
 }
