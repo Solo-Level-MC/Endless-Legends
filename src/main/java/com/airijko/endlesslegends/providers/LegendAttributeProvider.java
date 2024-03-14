400463e2d14dd38b990acd087483b5ca00e1ce68
Airijko <airijkoblade@gmail.com>
2024-03-14 02:50:11 -0500

    Interface logic changed

--- src/main/java/com/airijko/endlesslegends/providers/LegendAttributeProvider.java
+++ src/main/java/com/airijko/endlesslegends/providers/LegendAttributeProvider.java
@@ -15,36 +15,40 @@ public class LegendAttributeProvider implements AttributeModifierInterface {
     }
 
     @Override
-    public double getModifier(String attributeName, Player player) {
-        Legend legend = playerDataManager.getPlayerData(player.getUniqueId());
-        if (legend == null) {
-            return 0;
-        }
-        switch (attributeName) {
-            case "Life_Force":
-                return legend.lifeForce;
-            case "Strength":
-                return legend.strength;
-            case "Toughness":
-                return legend.toughness;
-            case "Knockback_Resistance":
-                return legend.knockbackResistance;
-            case "Movement_Speed":
-                return legend.speed;
-            case "Attack_Speed":
-                return legend.attackSpeed;
-            case "Precision":
-                return legend.precision;
-            case "Ferocity":
-                return legend.ferocity;
-            default:
-                return 0;
-        }
-    }
-    @Override
     public Map<String, Double> getModifiers(String attributeName, Player player) {
         Map<String, Double> attributeModifiers = new HashMap<>();
-        attributeModifiers.put(attributeName, getModifier(attributeName, player));
+        Legend legend = playerDataManager.getPlayerData(player.getUniqueId());
+        if (legend != null) {
+            switch (attributeName) {
+                case "Life_Force":
+                    attributeModifiers.put(attributeName, legend.lifeForce);
+                    break;
+                case "Strength":
+                    attributeModifiers.put(attributeName, legend.strength);
+                    break;
+                case "Toughness":
+                    attributeModifiers.put(attributeName, legend.toughness);
+                    break;
+                case "Knockback_Resistance":
+                    attributeModifiers.put(attributeName, legend.knockbackResistance);
+                    break;
+                case "Movement_Speed":
+                    attributeModifiers.put(attributeName, legend.speed);
+                    break;
+                case "Attack_Speed":
+                    attributeModifiers.put(attributeName, legend.attackSpeed);
+                    break;
+                case "Precision":
+                    attributeModifiers.put(attributeName, legend.precision);
+                    break;
+                case "Ferocity":
+                    attributeModifiers.put(attributeName, legend.ferocity);
+                    break;
+                default:
+                    attributeModifiers.put(attributeName, 0.0);
+                    break;
+            }
+        }
         return attributeModifiers;
     }
 
