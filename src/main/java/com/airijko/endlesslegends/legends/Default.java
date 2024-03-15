4ab709c9548c3b8505efa9dff213c726898f2f9c
Airijko <airijkoblade@gmail.com>
2024-03-15 06:55:30 -0500

    Created classes

--- src/main/java/com/airijko/endlesslegends/legends/Default.java
+++
@@ -1,8 +0,0 @@
-package com.airijko.endlesslegends.legends;
-
-public class Default  extends Legend{
-    public Default() {
-        super(20, 1, 0, 0, 0.1, 4, 0 , 0, Rank.NONE);
-    }
-
-}
--- src/main/java/com/airijko/endlesslegends/legends/DraftClass.java
+++
@@ -1,7 +0,0 @@
-package com.airijko.endlesslegends.legends;
-
-public class DraftClass extends Legend {
-    public DraftClass(Rank rank) {
-        super(50, 50, 10, 0.5, 0.1, 10, 50 , 100, rank);
-    }
-}
