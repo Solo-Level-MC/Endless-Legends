fc84957c4078c523c61668541d0d9e61d76b8494
Airijko <airijkoblade@gmail.com>
2024-03-17 02:33:16 -0500

    Adjust config

--- src/main/java/com/airijko/endlesslegends/settings/Config.java
+++ src/main/java/com/airijko/endlesslegends/settings/Config.java
@@ -1,10 +1,10 @@
 package com.airijko.endlesslegends.settings;
 public enum Config {
-    RANK_WEIGHTS("rank_weights", OptionType.DOUBLE),
     ALLOW_CLASS_CHANGE("allow_class_change", OptionType.BOOLEAN),
     CLASS_CHANGE_COOLDOWN("class_change_cooldown", OptionType.INT),
     RESET_CLASS_ON_DEATH("reset_class_on_death", OptionType.BOOLEAN),
-    REBORN_COOLDOWN("reborn_cooldown", OptionType.INT);
+    REBORN_COOLDOWN("reborn_cooldown", OptionType.INT),
+    AWAKEN_CHANCE("awaken_chance", OptionType.DOUBLE);
 
     private final String path;
     private final OptionType type;
